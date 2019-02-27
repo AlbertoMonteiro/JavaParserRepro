85978c1ad880b354d9e44cd442ab9ac3e994b0ea
Monteiro, Alberto - Dell Team <alberto_monteiro1@dellteam.com>
2019-02-27 09:45:11 -0300

    Added project

---
+++ src/app/SuperConfig.java
@@ -0,0 +1,11 @@
+package app;
+
+import app.infra.ConfigData;
+
+public class SuperConfig extends ConfigData {
+    public void Initialize() {
+        super.DoItInInfra(this);
+    }
+
+    public 
+}
---
+++ src/app/infra/ConfigData.java
@@ -0,0 +1,7 @@
+package app.infra;
+
+public class ConfigData {
+    public void DoItInInfra(ConfigData cfg) {
+
+    }
+}
